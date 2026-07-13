32a76c949ec89e08e61ad2f58845bcdd94469f0d
ilhome <ilkhom9799@gmail.com>
2026-07-13 19:02:16 +0900

    bootdev problems solved 18

--- python_file.py
+++ python_file.py
@@ -1,4 +1,8 @@
-stock_prices = [298,305,320,301,292]
-stock_prices.remove(1)
+stock_prices = []
+stock_prices.append(298)
+stock_prices.append(305)
+stock_prices.append(320)
+stock_prices.append(301)
+stock_prices.append(292)
 
 print(stock_prices)
